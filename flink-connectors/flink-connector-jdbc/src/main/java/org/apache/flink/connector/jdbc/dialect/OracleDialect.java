1d9db3db7b760b7c1486c3169e66d240ff74b637
mc.jeong <mc.jeong@navercorp.com>
2021-12-30 13:09:25 +0900

    WhitespaceAround Code Style Fix

--- flink-connectors/flink-connector-jdbc/src/main/java/org/apache/flink/connector/jdbc/dialect/OracleDialect.java
+++ flink-connectors/flink-connector-jdbc/src/main/java/org/apache/flink/connector/jdbc/dialect/OracleDialect.java
@@ -98,7 +98,7 @@ public class OracleDialect extends AbstractDialect {
                 + ")"
                 + " VALUES ("
                 + placeholders
-                +")";
+                + ")";
     }
 
     @Override
